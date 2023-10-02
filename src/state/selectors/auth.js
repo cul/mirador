8cadc62771f05a2cbc56cbdfe44fbe69d6bf1be8
Benjamin Armintor <armintor@gmail.com>
2023-10-02 13:32:01 -0400

    COLUMBIA: auth state selector should check non-image resources - IIIF Auth2 services will be members of a probe service - #3789

--- src/state/selectors/auth.js
+++ src/state/selectors/auth.js
@@ -1,6 +1,10 @@
 import { createSelector } from 'reselect';
 import { Utils } from 'manifesto.js';
 import flatten from 'lodash/flatten';
+import {
+  audioResourcesFrom, filterByTypes, textResourcesFrom, videoResourcesFrom,
+} from '../../lib/typeFilters';
+import MiradorCanvas from '../../lib/MiradorCanvas';
 import { miradorSlice, EMPTY_ARRAY, EMPTY_OBJECT } from './utils';
 import { getConfig } from './config';
 import { getVisibleCanvases, selectInfoResponses } from './canvases';
@@ -69,14 +73,26 @@ export const selectCurrentAuthServices = createSelector(
     }
 
     if (!currentAuthResources) return EMPTY_ARRAY;
+    if (currentAuthResources.length === 0 && canvases) {
+      currentAuthResources = flatten(canvases.map(c => {
+        const miradorCanvas = new MiradorCanvas(c);
+        const canvasResources = miradorCanvas.imageResources;
+        return videoResourcesFrom(canvasResources)
+          .concat(audioResourcesFrom(canvasResources))
+          .concat(textResourcesFrom(canvasResources));
+      }));
+    }
+
     if (currentAuthResources.length === 0) return EMPTY_ARRAY;
 
     const currentAuthServices = currentAuthResources.map(resource => {
       let lastAttemptedService;
-      const services = Utils.getServices(resource);
+      const resourceServices = Utils.getServices(resource);
+      const probeServices = filterByTypes(resourceServices, 'AuthProbeService2');
+      const probeServiceServices = flatten(probeServices.map(p => Utils.getServices(p)));
 
       for (const authProfile of serviceProfiles) {
-        const profiledAuthServices = services.filter(
+        const profiledAuthServices = resourceServices.concat(probeServiceServices).filter(
           p => authProfile.profile === p.getProfile(),
         );
 
