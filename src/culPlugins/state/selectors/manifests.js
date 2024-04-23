3651d3d1d79e2eea1f5c2f96c21508bce7a5a144
Benjamin Armintor <armintor@gmail.com>
2024-04-23 18:33:13 -0400

    COLUMBIA: Refactor download, related links, and XML plugins for consistency in rendering vs seeAlso - do not render canvas download list if canvas does not enable - consistent organization of selectors - deduplication of some selectors across plugins

--- src/culPlugins/state/selectors/manifests.js
+++ src/culPlugins/state/selectors/manifests.js
@@ -1,7 +1,9 @@
+import { PropertyValue } from 'manifesto.js';
 import { createSelector } from 'reselect';
+import asArray from '../../../lib/asArray';
 import MiradorCanvas from '../../../lib/MiradorCanvas';
 import canvasTypes from '../../../lib/canvasTypes';
-import { getCanvases, getManifestoInstance } from '../../../state/selectors';
+import { getCanvases, getManifestoInstance, getManifestLocale } from '../../../state/selectors';
 
 /**
  * Detect the IIIF v3 content types represented in the canvas
@@ -22,13 +24,60 @@ export const getCanvasesContentTypes = createSelector(
   },
 );
 
-export const getManifestDoi = createSelector(
+/**
+ * Get a named property for the current manifest
+ */
+const getManifestProperty = (property) => createSelector(
   [getManifestoInstance],
-  (manifest) => {
-    const doiValue = manifest?.getProperty('doi');
+  manifest => manifest && manifest.getProperty(property),
+);
+
+/**
+ * Get the id value for the manifest property
+ */
+export const getManifestDoi = createSelector(
+  [getManifestProperty('doi')],
+  (doiValue) => {
     if (!doiValue) return null;
     if (Object.prototype.hasOwnProperty.call(doiValue, 'id')) return doiValue.id;
     if (Object.prototype.hasOwnProperty.call(doiValue, '@id')) return doiValue['@id'];
     return doiValue.toString();
+  },
+);
+
+/**
+ * Build a destructuring selector for the given link property
+ */
+const linkSelector = (linkProperty) => createSelector(
+  [
+    getManifestProperty(linkProperty),
+    getManifestLocale,
+  ],
+  (linkPropertyValues, locale) => {
+    if (!linkPropertyValues) return null;
+    return asArray(linkPropertyValues).map(link => (
+      {
+        ...link,
+        format: link.format,
+        label: PropertyValue.parse(link.label, locale)
+          .getValue(),
+        value: link.id || link['@id'],
+      }
+    ));
+  },
+);
+
+export const getManifestRenderings = linkSelector('rendering');
+
+export const getManifestSeeAlso = linkSelector('seeAlso');
+
+export const getManifestRelatedLinks = createSelector(
+  [
+    getManifestRenderings,
+    getManifestSeeAlso,
+  ],
+  (renderings, seeAlso) => {
+    if (!renderings && !seeAlso) return null;
+    return asArray(renderings || []).concat(seeAlso || []);
   },
 );
