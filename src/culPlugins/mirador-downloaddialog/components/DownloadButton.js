1b0a453d397d8f65ee78ef08ebc5cc5851cf228a
Benjamin Armintor <armintor@gmail.com>
2024-04-24 18:24:55 -0400

    COLUMBIA: DownloadDialog plugin responds to presence of 'no-download' behavior on canvas

--- src/culPlugins/mirador-downloaddialog/components/DownloadButton.js
+++ src/culPlugins/mirador-downloaddialog/components/DownloadButton.js
@@ -1,26 +1,30 @@
+import Badge from '@mui/material/Badge';
 import DownloadIcon from '@mui/icons-material/VerticalAlignBottomSharp';
 import PropTypes from 'prop-types';
 import { MiradorMenuButton } from '../../../components/MiradorMenuButton';
 
 /** */
 const DownloadButton = ({
-  config, containerId, t, updateConfig,
+  config, containerId, suppressDownload, t, updateConfig,
 }) => {
   const { dialogOpen, enabled } = config;
   if (!enabled) {
     return null;
   }
+  const buttonLabel = suppressDownload ? 'limitedDownloadOptions' : 'showDownloadOptions';
   return (
     <MiradorMenuButton
       aria-expanded={dialogOpen}
       aria-haspopup
-      aria-label={t('showDownloadOptions')}
+      aria-label={t(buttonLabel)}
       onClick={() => updateConfig({
         ...config,
         dialogOpen: !dialogOpen,
       })}
     >
-      <DownloadIcon />
+      <Badge color="error" invisible={!suppressDownload} variant="dot">
+        <DownloadIcon />
+      </Badge>
     </MiradorMenuButton>
   );
 };
