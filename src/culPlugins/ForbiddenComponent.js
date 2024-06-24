2715ac92f789d360509d565bd639f9026cbe365c
Benjamin Armintor <armintor@gmail.com>
2024-06-24 09:37:49 -0400

    COLUMBIA: fix text spacing in culPlugins/ForbiddenComponent

--- src/culPlugins/ForbiddenComponent.js
+++ src/culPlugins/ForbiddenComponent.js
@@ -4,8 +4,7 @@ const ForbiddenComponent = (props) => {
   return (
     <div className="alert alert-warning w-100 d-flex">
       <h3 className="my-auto mx-auto h6">
-        you do not currently have access to
-        {id}
+        {`you do not currently have access to ${id}`}
       </h3>
     </div>
   );
