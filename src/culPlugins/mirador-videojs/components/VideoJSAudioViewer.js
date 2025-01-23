a7f270eb7e3ae73efd71ac1995a36bb07ee0f9a0
Benjamin Armintor <armintor@gmail.com>
2025-01-23 17:53:54 -0500

    COLUMBIA: update src/culPlugins/mirador-videojs for v4 and vite - linter fixes for src/culPlugins/mirador-videojs/components/VideoJS.js - linter fixes for src/culPlugins/mirador-videojs/components/VideoJSAudioViewer.js - linter fixes for src/culPlugins/mirador-videojs/components/VideoJSViewer.js

--- src/culPlugins/mirador-videojs/components/VideoJSAudioViewer.js
+++ src/culPlugins/mirador-videojs/components/VideoJSAudioViewer.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
 import { getConfig, getVisibleCanvasCaptions, getVisibleCanvasAudioResources } from '../../../state/selectors';
 
 import { VideoJS } from './VideoJS';
@@ -22,35 +23,39 @@ const enhance = compose(
 );
 
 /** */
-class VideoJSAudioViewerBase extends Component {
-  /** */
-  render() {
-    const {
-      captions, audioOptions, audioResources,
-    } = this.props;
+const VideoJSAudioViewerBase = ({
+  audioOptions, audioResources, captions,
+}) => {
+  const videoJsOptions = {
+    ...audioOptions,
+    autoplay: false,
+    controlBar: {
+      remainingTimeDisplay: false,
+    },
+    controls: true,
+    fill: true,
+    playbackRates: [0.5, 1, 1.5, 2],
+    responsive: true,
+    sources: audioResources.filter(audio => audio.id && audio.getFormat()).map(
+      audio => ({ src: audio.id, type: audio.getFormat() }),
+    ),
+    tracks: captions.filter(caption => caption.id).map(caption => ({ kind: (caption.kind || 'captions'), src: caption.id })),
+  };
 
-    const videoJsOptions = {
-      ...audioOptions,
-      autoplay: false,
-      controlBar: {
-        remainingTimeDisplay: false,
-      },
-      controls: true,
-      fill: true,
-      playbackRates: [0.5, 1, 1.5, 2],
-      responsive: true,
-      sources: audioResources.filter(audio => audio.id && audio.getFormat()).map(
-        audio => ({ src: audio.id, type: audio.getFormat() }),
-      ),
-      tracks: captions.filter(caption => caption.id).map(caption => ({ kind: (caption.kind || 'captions'), src: caption.id })),
-    };
-
-    if (videoJsOptions.sources.length === 0) return <ForbiddenComponent id="this content" />;
-    return <VideoJS options={videoJsOptions} />;
-  }
-}
+  if (videoJsOptions.sources.length === 0) return <ForbiddenComponent id="this content" />;
+  return <VideoJS options={videoJsOptions} />;
+};
 
 export const VideoJSAudioViewer = enhance(VideoJSAudioViewerBase);
 
 /** */
-export default ({ _targetComponent, targetProps }) => (<VideoJSAudioViewer {...targetProps} />);
+const VideoJSAudioViewerPlugin = ({ targetProps }) => (<VideoJSAudioViewer {...targetProps} />); // eslint-disable-line react/prop-types
+/** */
+export default VideoJSAudioViewerPlugin;
+
+VideoJSAudioViewerBase.propTypes = {
+  audioOptions: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  audioResources: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  captions: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+
+};
