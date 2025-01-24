83d312238b1d6ad1a0ee71d23021157043fd612d
Benjamin Armintor <armintor@gmail.com>
2025-01-23 21:55:49 -0500

    COLUMBIA: update src/culPlugins/mirador-pageIconViewerNavigation for v4 and vite - update __tests__/src/culPlugins/mirador-pageIconViewerNavigation/components/PageIconViewerNavigation.test.js for vite - convert PageIconViewerNavigation to functional component and use translation hook - PageIconViewerNavigation.test.js verifies translated values

--- src/culPlugins/mirador-pageIconViewerNavigation/components/PageIconViewerNavigation.js
+++ src/culPlugins/mirador-pageIconViewerNavigation/components/PageIconViewerNavigation.js
@@ -1,13 +1,17 @@
 import { Component } from 'react';
+import { useTranslation } from 'react-i18next';
 import LtrNextPage from '@mui/icons-material/AutoStories';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import MiradorMenuButton from '../../../containers/MiradorMenuButton';
 import ns from '../../../config/css-ns';
-
+/** */
+const noOp = () => {};
 /**
  */
-export class PageIconViewerNavigation extends Component {
+export function PageIconViewerNavigation({
+  hasNextCanvas = false, hasPreviousCanvas = false, setNextCanvas = noOp, setPreviousCanvas = noOp, viewingDirection = '',
+}) {
   /**
    * Renders previous next canvas buttons, but only if there are such canvases
    * The AutoStories icon appears to be a page turning, and is thus flipped
@@ -15,58 +19,53 @@ export class PageIconViewerNavigation extends Component {
    * for the reading direction can thus be applied uniformly to both but before
    * the scale transform.
    */
-  render() {
-    const {
-      hasNextCanvas, hasPreviousCanvas, setNextCanvas, setPreviousCanvas, t,
-      viewingDirection,
-    } = this.props;
+  const { t } = useTranslation();
 
-    if (!hasPreviousCanvas && !hasNextCanvas) return (null);
-    let htmlDir = 'ltr';
-    const prevTransform = ['scale(-1, 1)'];
-    const nextTransform = [];
-    switch (viewingDirection) {
-      case 'top-to-bottom':
-        prevTransform.unshift('rotate(90deg)');
-        nextTransform.unshift('rotate(90deg)');
-        break;
-      case 'bottom-to-top':
-        prevTransform.unshift('rotate(270deg)');
-        nextTransform.unshift('rotate(270deg)');
-        break;
-      case 'right-to-left':
-        htmlDir = 'rtl';
-        nextTransform.push(prevTransform.pop());
-        break;
-      default:
-        break;
-    }
-    const previousIconStyle = (prevTransform.length > 0) ? { transform: prevTransform.join(' ') } : {};
-    const nextIconStyle = (nextTransform.length > 0) ? { transform: nextTransform.join(' ') } : {};
-    return (
-      <div
-        className={classNames(ns('osd-navigation'))}
-        dir={htmlDir}
-      >
-        <MiradorMenuButton
-          aria-label={t('previousCanvas')}
-          className={ns('previous-canvas-button')}
-          disabled={!hasPreviousCanvas}
-          onClick={() => { hasPreviousCanvas && setPreviousCanvas(); }}
-        >
-          <LtrNextPage style={previousIconStyle} />
-        </MiradorMenuButton>
-        <MiradorMenuButton
-          aria-label={t('nextCanvas')}
-          className={ns('next-canvas-button')}
-          disabled={!hasNextCanvas}
-          onClick={() => { hasNextCanvas && setNextCanvas(); }}
-        >
-          <LtrNextPage style={nextIconStyle} />
-        </MiradorMenuButton>
-      </div>
-    );
+  if (!hasPreviousCanvas && !hasNextCanvas) return (null);
+  let htmlDir = 'ltr';
+  const prevTransform = ['scale(-1, 1)'];
+  const nextTransform = [];
+  switch (viewingDirection) {
+    case 'top-to-bottom':
+      prevTransform.unshift('rotate(90deg)');
+      nextTransform.unshift('rotate(90deg)');
+      break;
+    case 'bottom-to-top':
+      prevTransform.unshift('rotate(270deg)');
+      nextTransform.unshift('rotate(270deg)');
+      break;
+    case 'right-to-left':
+      htmlDir = 'rtl';
+      nextTransform.push(prevTransform.pop());
+      break;
+    default:
+      break;
   }
+  const previousIconStyle = (prevTransform.length > 0) ? { transform: prevTransform.join(' ') } : {};
+  const nextIconStyle = (nextTransform.length > 0) ? { transform: nextTransform.join(' ') } : {};
+  return (
+    <div
+      className={classNames(ns('osd-navigation'))}
+      dir={htmlDir}
+    >
+      <MiradorMenuButton
+        aria-label={t('previousCanvas')}
+        className={ns('previous-canvas-button')}
+        disabled={!hasPreviousCanvas}
+        onClick={() => { hasPreviousCanvas && setPreviousCanvas(); }}
+      >
+        <LtrNextPage style={previousIconStyle} />
+      </MiradorMenuButton>
+      <MiradorMenuButton
+        aria-label={t('nextCanvas')}
+        className={ns('next-canvas-button')}
+        disabled={!hasNextCanvas}
+        onClick={() => { hasNextCanvas && setNextCanvas(); }}
+      >
+        <LtrNextPage style={nextIconStyle} />
+      </MiradorMenuButton>
+    </div>
+  );
 }
 
 PageIconViewerNavigation.propTypes = {
@@ -74,14 +73,5 @@ PageIconViewerNavigation.propTypes = {
   hasPreviousCanvas: PropTypes.bool,
   setNextCanvas: PropTypes.func,
   setPreviousCanvas: PropTypes.func,
-  t: PropTypes.func.isRequired,
   viewingDirection: PropTypes.string,
-};
-
-PageIconViewerNavigation.defaultProps = {
-  hasNextCanvas: false,
-  hasPreviousCanvas: false,
-  setNextCanvas: () => {},
-  setPreviousCanvas: () => {},
-  viewingDirection: '',
 };
