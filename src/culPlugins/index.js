d8088edec1a4f3aa005cd5be28c0750d6214e1a5
Benjamin Armintor <armintor@gmail.com>
2025-01-28 18:20:22 -0500

    COLUMBIA: src/culPlugins/mirador-nativeObjectViewer plugin for text resources - Rudimentary fullscreen resizing for NativeObjectViewer - NativeObjectViewer use a ResizeObserver effect directly

--- src/culPlugins/index.js
+++ src/culPlugins/index.js
@@ -4,6 +4,7 @@ import citationsSidebarPlugin from './mirador-citations';
 import collectionFoldersPlugin from './mirador-selectCollectionFolders';
 import downloadDialogPlugin from './mirador-downloaddialog';
 import hintingSideBar from './mirador-hinting-sidebar';
+import nativeObjectViewerPlugin from './mirador-nativeObjectViewer';
 import videojsPlugin from './mirador-videojs';
 import viewerNavigation from './mirador-pageIconViewerNavigation';
 import viewXmlPlugin from './mirador-viewXml';
@@ -15,6 +16,7 @@ export default {
   collectionFoldersPlugin,
   downloadDialogPlugin,
   hintingSideBar,
+  nativeObjectViewerPlugin,
   videojsPlugin,
   viewerNavigation,
   viewXmlPlugin,
