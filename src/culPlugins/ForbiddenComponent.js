a8592ad3d57c8ac445523cdc3246287d122b2a95
Benjamin Armintor <armintor@gmail.com>
2025-02-01 11:27:45 -0500

    COLUMBIA: linter fixes for src/culPlugins/ForbiddenComponent.js

--- src/culPlugins/ForbiddenComponent.js
+++ src/culPlugins/ForbiddenComponent.js
@@ -1,13 +1,16 @@
+import PropTypes from 'prop-types';
+
 /** */
-const ForbiddenComponent = (props) => {
-  const { id } = props;
-  return (
-    <div className="alert alert-warning w-100 d-flex">
-      <h3 className="my-auto mx-auto h6">
-        {`you do not currently have access to ${id}`}
-      </h3>
-    </div>
-  );
+const ForbiddenComponent = ({ id = null }) => (
+  <div className="alert alert-warning w-100 d-flex">
+    <h3 className="my-auto mx-auto h6">
+      {`you do not currently have access to ${id}`}
+    </h3>
+  </div>
+);
+
+ForbiddenComponent.propTypes = {
+  id: PropTypes.string,
 };
 
 export default ForbiddenComponent;
