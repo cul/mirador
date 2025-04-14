fa652a7c7a0391ca68ade6abfec446101358ce04
Benjamin Armintor <armintor@gmail.com>
2025-04-14 11:22:04 -0400

    COLUMBIA: add mirador-videojs plugin - Use videojs fill in a 100% container instead of fluid for responsive sizing - linted for v4 and vite

---
+++ src/culPlugins/mirador-videojs/components/VideoJSViewer.jsx
@@ -0,0 +1,57 @@
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { VideoViewer } from '../../../components/VideoViewer';
+import { getConfig, getVisibleCanvasCaptions, getVisibleCanvasVideoResources } from '../../../state/selectors';
+
+import { VideoJS } from './VideoJS';
+import ForbiddenComponent from '../../ForbiddenComponent';
+
+/** */
+const mapStateToProps = (state, { windowId }) => (
+  {
+    captions: getVisibleCanvasCaptions(state, { windowId }) || [],
+    videoOptions: getConfig(state).videoOptions,
+    videoResources: getVisibleCanvasVideoResources(state, { windowId }) || [],
+  }
+);
+
+const enhance = compose(
+  withTranslation(),
+  connect(mapStateToProps, null),
+);
+
+/** */
+function VideoJSViewerBase({ captions, videoOptions, videoResources }) {
+  const videoJsOptions = {
+    ...videoOptions,
+    autoplay: false,
+    controlBar: {
+      remainingTimeDisplay: false,
+    },
+    controls: true,
+    fill: true,
+    playbackRates: [0.5, 1, 1.5, 2],
+    responsive: true,
+    sources: videoResources.filter(video => video.id && video.getFormat()).map(
+      video => ({ src: video.id, type: video.getFormat() }),
+    ),
+    tracks: captions.filter(caption => caption.id).map(caption => ({ kind: (caption.kind || 'captions'), src: caption.id })),
+  };
+
+  if (videoJsOptions.sources.length === 0) return <ForbiddenComponent id="this content" />;
+  return <VideoJS options={videoJsOptions} />;
+}
+
+export const VideoJSViewer = enhance(VideoJSViewerBase);
+/** */
+const VideoJSViewerPlugin = ({ targetProps }) => (<VideoJSViewer {...targetProps} />); // eslint-disable-line react/prop-types
+/** */
+export default VideoJSViewerPlugin;
+
+VideoJSViewerBase.propTypes = {
+  captions: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  videoOptions: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  videoResources: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+};
