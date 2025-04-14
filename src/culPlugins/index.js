55b7792b666a1e759b606b07b355613601dbdda1
Benjamin Armintor <armintor@gmail.com>
2025-04-14 11:22:56 -0400

    COLUMBIA: export videojs plugin from culPlugins

--- src/culPlugins/index.js
+++ src/culPlugins/index.js
@@ -2,10 +2,12 @@ import canvasLinkPlugin from './mirador-canvaslink';
 import canvasRelatedLinksPlugin from './mirador-canvasRelatedLinks';
 import citationsSidebarPlugin from './mirador-citations';
 import downloadDialogPlugin from './mirador-downloaddialog';
+import videojsPlugin from './mirador-videojs';
 
 export default {
   canvasLinkPlugin,
   canvasRelatedLinksPlugin,
   citationsSidebarPlugin,
   downloadDialogPlugin,
+  videojsPlugin,
 };
